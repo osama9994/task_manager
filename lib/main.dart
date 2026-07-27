e12ab60afdff839d630a61bf0fca9052f523fca8
osama9994 <alshalbeosama6@gmail.com>
2026-07-27 19:45:04 +0300

    Configure Firebase and update CI CD workflows

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:task_manager/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // تهيئة الفايربيس
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
